93817c6be693d1ee6abe774a7e91bf3879161e0a
shahalcm <shahalcm9@gmail.com>
2026-06-29 13:12:25 +0530

    secound updates

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://velora-cart-server.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
